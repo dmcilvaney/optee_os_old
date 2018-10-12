4e1db394208cc243ebdb21ee7e111837e662787f
Daniel McIlvaney <damcilva@microsoft.com>
2018-10-12 13:43:02 -0700

    Improve thread safety of hw_get_random_byte

--- core/arch/arm/plat-imx/imx_rng.c
+++ core/arch/arm/plat-imx/imx_rng.c
@@ -1,5 +1,6 @@
 #include <fsl_caam/fsl_sec.h>
 #include <io.h>
+#include <kernel/mutex.h>
 #include <kernel/panic.h>
 #include <mm/core_mmu.h>
 #include <mm/core_memprot.h>
@@ -9,22 +10,30 @@
 
 #define CAAM_RANDOM_BUFFER_SIZE 0x100
 
+static unsigned int rng_lock = SPINLOCK_UNLOCK;
+static uint8_t random_buffer[CAAM_RANDOM_BUFFER_SIZE];
+static volatile size_t available = 0;
+
 uint8_t hw_get_random_byte(void)
 {
-    static uint8_t random_buffer[CAAM_RANDOM_BUFFER_SIZE];
-    static size_t available = 0;
-    int ret = TEE_SUCCESS;
+	int ret = TEE_SUCCESS;
+	uint8_t random_value;
 
-    if(available <= 0) {
-        FMSG("Refilling RNG buffer");
-        ret = fsl_get_random_bytes(random_buffer, CAAM_RANDOM_BUFFER_SIZE);
-        if (ret) {
-            EMSG("Failed to get random bytes: %x", ret);
-            panic();
-        }
-        available = CAAM_RANDOM_BUFFER_SIZE;
-    }
+	cpu_spin_lock(&rng_lock);
+	if(available <= 0) {
+		FMSG("Refilling RNG buffer");
+		ret = fsl_get_random_bytes(random_buffer,
+				 CAAM_RANDOM_BUFFER_SIZE);
+		if (ret) {
+			EMSG("Failed to get random bytes: %x", ret);
+			panic();
+		}
+		available = CAAM_RANDOM_BUFFER_SIZE;
+	}
 
-    available -= 1;
-    return random_buffer[available];
+	available -= 1;
+	random_value = random_buffer[available];
+	cpu_spin_unlock(&rng_lock);
+
+	return random_value;
 }
