261acd98365e516539d815cea38f03519790bcf6
Daniel McIlvaney <damcilva@microsoft.com>
2018-10-12 11:19:51 -0700

    Remove mentions of Cyres work, clean up failure flow

--- core/arch/arm/plat-imx/main.c
+++ core/arch/arm/plat-imx/main.c
@@ -46,7 +46,7 @@
 #include <tee/entry_fast.h>
 #include <tee/entry_std.h>
 
-#ifdef CFG_CYRES
+#ifdef CFG_SPL_IDENTITY_SEED
 #include <crypto/crypto.h>
 #include <imx_cdi.h>
 #endif
@@ -116,7 +116,7 @@ register_phys_mem(MEM_AREA_IO_SEC,
 #endif
 
 register_phys_mem(MEM_AREA_IO_SEC, CAAM_BASE, CORE_MMU_DEVICE_SIZE);
-#ifdef CFG_CYRES
+#ifdef CFG_SPL_IDENTITY_SEED
 register_phys_mem(MEM_AREA_IO_SEC,
 	CAAM_SECURE_MEMORY_BASE,
 	CAAM_SECURE_MEMORY_PAGE_SIZE * 4);
@@ -303,37 +303,7 @@ static TEE_Result init_tzc380(void)
 service_init(init_tzc380);
 #endif // #ifdef CFG_TZC380
 
-#ifdef CFG_CYRES
-
-TEE_Result imx_was_cdi_successful(void)
-{
-	TEE_Result res = TEE_SUCCESS;
-	struct cyres_identity_seed *unique_id =
-		(struct cyres_identity_seed *)phys_to_virt(
-						CYRES_IDENTITY_SEED_ADDR,
-						MEM_AREA_IO_SEC);
-
-	if (unique_id->magic != CYRES_IDENTITY_SEED_MAGIC) {
-		EMSG("Bad Cyres CDI magic, found 0x%x but was expecting 0x%x",
-				unique_id->magic,
-				CYRES_IDENTITY_SEED_MAGIC);
-		panic();
-	}
-	if (unique_id->version != CYRES_IDENTITY_SEED_VERSION) {
-		EMSG("Bad Cyres CDI version, found 0x%x but was expecting 0x%x",
-				unique_id->version,
-				CYRES_IDENTITY_SEED_VERSION);
-		panic();
-	}
-
-	if (!unique_id->cdi_valid) {
-		EMSG("SPL reports invalid CDI.");
-		EMSG("Ensure that High Assurance Boot (HAB) is enabled.");
-		EMSG("Try OP-TEE with CFG_RPMB_TESTKEY=y to skip CDI.");
-		res = TEE_ERROR_SECURITY;
-	}
-	return res;
-}
+#ifdef CFG_SPL_IDENTITY_SEED
 
 /*
  * SPL places a device identity derived from the CAAM's unique
@@ -348,34 +318,31 @@ TEE_Result tee_otp_get_hw_unique_key(struct tee_hw_unique_key *hwkey)
 	void *ctx = NULL;
 	static uint8_t string_for_unique_key_gen[] = "imx_unique_key_gen";
 
-	struct cyres_identity_seed *unique_id =
-		(struct cyres_identity_seed *)phys_to_virt(
-						CYRES_IDENTITY_SEED_ADDR,
+	struct spl_identity_seed *unique_id =
+		(struct spl_identity_seed *)phys_to_virt(
+						SPL_IDENTITY_SEED_ADDR,
 						MEM_AREA_IO_SEC);
 
-	if (unique_id->magic != CYRES_IDENTITY_SEED_MAGIC) {
-		EMSG("Bad Cyres CDI magic, found 0x%x but was expecting 0x%x",
+	if (unique_id->magic != SPL_IDENTITY_SEED_MAGIC) {
+		EMSG("Bad SPL ID magic, found 0x%x but was expecting 0x%x",
 				unique_id->magic,
-				CYRES_IDENTITY_SEED_MAGIC);
+				SPL_IDENTITY_SEED_MAGIC);
 		panic();
 	}
-	if (unique_id->version != CYRES_IDENTITY_SEED_VERSION) {
-		EMSG("Bad Cyres CDI version, found 0x%x but was expecting 0x%x",
+	if (unique_id->version != SPL_IDENTITY_SEED_VERSION) {
+		EMSG("Bad SPL ID version, found 0x%x but was expecting 0x%x",
 				unique_id->version,
-				CYRES_IDENTITY_SEED_VERSION);
+				SPL_IDENTITY_SEED_VERSION);
 		panic();
 	}
-
-	if (imx_was_cdi_successful() != TEE_SUCCESS)
-		EMSG("SPL was unable to derive a unique ID from CAAM.");
 
 	res = crypto_mac_alloc_ctx(&ctx, TEE_ALG_HMAC_SHA256);
 	if (res)
 		goto err;
 
 	res = crypto_mac_init(ctx, TEE_ALG_HMAC_SHA256,
-				unique_id->hashed_cdi,
-				sizeof(unique_id->hashed_cdi));
+				unique_id->hashed_device_id,
+				sizeof(unique_id->hashed_device_id));
 	if (res)
 		goto err;
 
@@ -395,8 +362,12 @@ TEE_Result tee_otp_get_hw_unique_key(struct tee_hw_unique_key *hwkey)
 	 * If HAB is not enabled the CDI will not be correct.
 	 * This error is only checked when writing the RPMB key.
 	 */
-	if (imx_was_cdi_successful() != TEE_SUCCESS)
+	if (!unique_id->id_valid) {
+		EMSG("SPL reports invalid device ID.");
+		EMSG("Ensure that High Assurance Boot (HAB) is enabled.");
+		EMSG("Try OP-TEE with CFG_RPMB_TESTKEY=y to skip HW ID.");
 		res = TEE_ERROR_SECURITY;
+	}
 
 err:
 	if (ctx != NULL)
@@ -414,4 +385,4 @@ err:
  */
 //int tee_otp_get_die_id(uint8_t *buffer, size_t len)
 
-#endif // CFG_CYRES
+#endif // CFG_SPL_IDENTITY_SEED
